31ed1bcc95d93dc84296a63e75aceb57940853e6
bigdub3298 <worldwidewes@Wesleys-MacBook-Pro.local>
2016-09-26 13:31:08 -0600

    Adds View Functionality

---
+++ Post/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Post
+//
+//  Created by Wesley Austin on 9/25/16.
+//  Copyright © 2016 Wesley Austin. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
