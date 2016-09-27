bf1660287ea807131a42a61a598dd6c2382ca978
bigdub3298 <worldwidewes@Wesleys-MacBook-Pro.local>
2016-09-26 23:44:38 -0600

    Adds post message functionality
    When the add button is pressed it presents an AlertController that takes input for the username and message.

--- Post/PostListTableViewController.swift
+++ Post/PostListTableViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class PostListTableViewController: UITableViewController {
+class PostListTableViewController: UITableViewController, PostControllerDelegate {
 
+    var postController = PostController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        postController.delegate = self
+        
+        
+        refreshControl?.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl?.addTarget(self, action: #selector(refresh), forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,26 +30,118 @@ class PostListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return postController.posts.count
     }
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("postCells", forIndexPath: indexPath) as! PostTableViewCell
 
-        // Configure the cell...
-
+        let post = postController.posts[indexPath.row]
+        cell.postTextLabel.text = post.text
+        let df = NSDateFormatter()
+        df.dateStyle = .MediumStyle
+        df.timeStyle = .ShortStyle
+        
+        cell.postTimestampLabel.text = df.stringFromDate(NSDate(timeIntervalSince1970: post.timestamp))
+        
         return cell
     }
-    */
+ 
 
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    // MARK: - PostController Delegate 
+    
+    func postsUpdated(posts: [Post]) {
+        tableView.reloadData()
+    }
+    
+    func refresh(refreshControl: UIRefreshControl) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        postController.fetchPosts { (posts) in
+            dispatch_async(dispatch_get_main_queue(), {
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                refreshControl.endRefreshing()
+                self.tableView.reloadData()
+            })
+        }
+    }
+    
+    // MARK: - Actions 
+    
+    @IBAction func addPostButtonTapped(sender: UIBarButtonItem) {
+        presentNewPostAlert()
+    }
+    
+    // MARK: - Helper function
+    func presentNewPostAlert() {
+        let addNotif = UIAlertController(title: "Add Post", message: nil, preferredStyle: .Alert)
+        
+        addNotif.addTextFieldWithConfigurationHandler { (textField) in
+            textField.attributedPlaceholder = NSAttributedString(string: "username")
+        }
+    
+        addNotif.addTextFieldWithConfigurationHandler { (textField) in
+            textField.attributedPlaceholder = NSAttributedString(string: "message")
+        }
+    
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        let postAction = UIAlertAction(title: "Post", style: .Default) { (_) in
+            let usernameTextField = addNotif.textFields![0]
+            let messageTextField = addNotif.textFields![1]
+            
+            if usernameTextField.text != "" && messageTextField.text != "" {
+                let username = usernameTextField.text!
+                let message = messageTextField.text!
+                
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+                self.postController.addPost(username, text: message, completion: { (success) in
+                    
+                    
+                    self.postController.fetchPosts({ (posts) in
+                        dispatch_async(dispatch_get_main_queue(), {
+                            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                            self.tableView.reloadData()
+                        })
+                    })
+                    print(success)
+                })
+            } else {
+
+                    self.presentErrorAlert()
+                    return
+            }
+            
+            
+        }
+        
+        addNotif.addAction(cancelAction)
+        addNotif.addAction(postAction)
+        
+        self.presentViewController(addNotif, animated: true, completion: nil)
+    }
+    
+    func presentErrorAlert() {
+        let errorNotif = UIAlertController(title: "Missing either username or message", message: nil, preferredStyle: .Alert)
+        
+        let tryAgainAction = UIAlertAction(title: "Try Again", style: .Default) { (_) in
+                self.presentNewPostAlert()
+        }
+        
+        errorNotif.addAction(tryAgainAction)
+        
+        self.presentViewController(errorNotif, animated: true, completion: nil)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
